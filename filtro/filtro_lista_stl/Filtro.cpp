5a4ac4ccc10e0ab33cbe0299b25c0123212a1a58
alxperezECI <alexander.perez@escuelaing.edu.co>
2022-03-15 10:56:47 -0500

    Agregando el punto de partida del ejercicio del filtro pero haciendo uso de la lista de la biblioteca estándar.

---
+++ filtro/filtro_lista_stl/Filtro.cpp
@@ -0,0 +1,42 @@
+/**
+ * Project Untitled
+ */
+
+
+#include "Filtro.h"
+#include <iostream>
+
+/**
+ * Filtro implementation
+ */
+Filtro::Filtro(unsigned char tamano){
+    _tamano = tamano;
+    _memoria.clear();
+}
+
+/**
+ * @param a
+ * @return void
+ */
+bool Filtro::agregarDato(float a) {
+	// Escrir el código para garantizar el tamaño fijo de la ventana de muestras
+    return true;
+}
+
+/**
+ * @return float
+ */
+float Filtro::promedio() {
+	// Escribir el código para calcular el promedio de los datos de la lista
+
+    return prom;
+}
+
+float& Filtro::salida_filtro(float &a){
+	float prom = promedio();
+	agregarDato( a );
+	return (a -= prom);
+}
+
+
+
