fb251bd5c536a551d70ecb1733050bc668499fb3
Alexander Pérez Ruiz <alexander.perez@escuelaing.edu.co>
2022-03-23 16:07:23 -0500

    Ampliando el ejemplo de GUI con Qt.

--- qt_ejemplo/qt_matriz/ventppal.cpp
+++ qt_ejemplo/qt_matriz/ventppal.cpp
@@ -1,5 +1,6 @@
 #include "ventppal.h"
 #include "./ui_ventppal.h"
+#include <string>
 
 VentPPal::VentPPal(QWidget *parent)
     : QMainWindow(parent)
@@ -13,3 +14,21 @@ VentPPal::~VentPPal()
     delete ui;
 }
 
+/*
+ *
+int main()
+{
+  MatrixXd m(2,2);
+  m(0,0) = 3;
+  m(1,0) = 2.5;
+  m(0,1) = -1;
+  m(1,1) = m(1,0) + m(0,1);
+  std::cout << m << std::endl;
+}
+*/
+void VentPPal::on_pushButton_clicked()
+{
+    QString a = ui->txt_M1_11->text();
+    ui->txt_MR_11->setText("Hola Alse");
+    ui->txt_MR_11->insert( a  );
+}
--- qt_ejemplo/qt_matriz/ventppal.h
+++ qt_ejemplo/qt_matriz/ventppal.h
@@ -2,6 +2,10 @@
 #define VENTPPAL_H
 
 #include <QMainWindow>
+#include <iostream>
+#include <eigen3/Eigen/Dense>
+
+using Eigen::MatrixXd;
 
 QT_BEGIN_NAMESPACE
 namespace Ui { class VentPPal; }
@@ -15,7 +19,14 @@ public:
     VentPPal(QWidget *parent = nullptr);
     ~VentPPal();
 
+private slots:
+    void on_pushButton_clicked();
+
 private:
     Ui::VentPPal *ui;
+    Eigen::MatrixXd m1(2,2);
+    MatrixXd m2(2,2);
+    MatrixXd mr(2,2);
+
 };
 #endif // VENTPPAL_H
